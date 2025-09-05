f43751d8f2925719ade075f1fe55013f37cbbb8a
Vikash-Kumar001 <vikashkumarsudhi8527@gmail.com>
2025-09-05 22:22:07 +0530

    feat: Project frontend setup using Next.js (TypeScript) + Tailwind CSS

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col gap-4 bg-green-500">
+      <div className="text-3xl font-bold text-center text-white">Welcome to ZeroPrint Core</div>
+    </div>
+  );
+}
